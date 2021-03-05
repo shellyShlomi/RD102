dddb3783ce8a786d9bfc8d6cf68dd6f601aab1e5
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-03-05 21:55:00 +0200

    ex4

--- c/ws1/ex4.c
+++ c/ws1/ex4.c
@@ -1,10 +1,8 @@
 #include <stddef.h>
 #include <stdio.h>
 
-void print_hello_world(char arr[])
-{
-    printf("%s\n", arr);
-}
+
+void print_hello_world(char arr[]);
 
 
 int main()
@@ -13,12 +11,13 @@ int main()
     char arr[] = {0x22, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x22, 0x0};
     print_hello_world(arr);
 
-
-
-    
     return 0;
 }
 
+void print_hello_world(char arr[])
+{
+    printf("%s\n", arr);
+}
 
 // void print_hello_world_int(int arr[])
 // {
---
+++ c/ws1/ex5.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+double pow_for_10(int n);
+
+int main()
+{
+
+    int a = 0;
+    double exp_a = 1;
+    if (exp_a != pow_for_10(a))
+    {
+        printf("the num is %d\n the expected num is %f\n", a, exp_a);
+        printf("ERROR\n");
+    }
+    else
+    {
+        printf("the num is %d\n the expected num is %f\n", a, exp_a);
+        printf("YESSSSSS\n");
+    }
+
+    a = -4;
+    exp_a = (0.0001);
+    if ((float)exp_a != (float)pow_for_10(a))
+    {
+        printf("the num is %d\n the expected num is %f\n", a, exp_a);
+        printf("%f ERROR\n", pow_for_10(a));
+    }
+    else
+    {
+        printf("the num is %d\n the expected num is %f\n", a, exp_a);
+        printf("YESSSSSS\n");
+    }
+
+    a = 4;
+    exp_a = (10000);
+    if (exp_a != pow_for_10(a))
+    {
+        printf("the num is %d\n the expected num is %f\n", a, exp_a);
+        printf("ERROR\n");
+    }
+    else
+    {
+        printf("the num is %d\n the expected num is %f\n", a, exp_a);
+        printf("YESSSSSS\n");
+    }
+
+    return 0;
+}
+
+double pow_for_10(int n)
+{
+    double base = 10;
+    double ans = 1;
+    int i;
+    if (n < 0)
+    {
+        base = (1 / base);
+        n = -(n);
+    }
+    
+    for (i = 0; i < n; i++)
+    {
+        ans = base * ans;
+    }
+    return ans;
+}
