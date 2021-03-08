e3133e569786d12729223739ae9f92aa575177c6
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-03-08 15:02:05 +0200

    ws1 ex5 after CR

--- c/ws1/ex5.c
+++ c/ws1/ex5.c
@@ -1,66 +1,74 @@
 #include <stdio.h>
 
-double pow_for_10(int n);
+double PowOfTeen(int n);
 
 int main()
 {
-
     int a = 0;
     double exp_a = 1;
-    if (exp_a != pow_for_10(a))
+    
+    if (exp_a != PowOfTeen(a))
     {
-        printf("the num is %d and the return value is %f\n the expected num is %f\n", a, pow_for_10(a), exp_a);
+        printf("the num is %d and the return value is %f\n the expected num is %f\n"
+        	, a, PowOfTeen(a), exp_a);
         printf("ERROR\n");
     }
     else
     {
-        printf("the num is %d and the return value is %f\n the expected num is %f\n", a, pow_for_10(a), exp_a);
+        printf("the num is %d and the return value is %f\n the expected num is %f\n"
+        	, a, PowOfTeen(a), exp_a);
         printf("YESSSSSS\n");
     }
 
     a = -6;
     exp_a = (0.000001);
-    if ((float)exp_a != (float)pow_for_10(a))
+    if ((float)exp_a != (float)PowOfTeen(a))
     {
-        printf("the num is %d and the return value is %f\n the expected num is %f\n", a, pow_for_10(a), exp_a);
-        printf("%f ERROR\n", pow_for_10(a));
+        printf("the num is %d and the return value is %f\n the expected num is %f\n"
+        	, a, PowOfTeen(a), exp_a);
+        printf("%f ERROR\n", PowOfTeen(a));
     }
     else
     {
-        printf("the num is %d and the return value is %f\n the expected num is %f\n", a, pow_for_10(a), exp_a);
+        printf("the num is %d and the return value is %f\n the expected num is %f\n"
+        	, a, PowOfTeen(a), exp_a);
         printf("YESSSSSS\n");
     }
 
     a = 4;
     exp_a = (10000);
-    if (exp_a != pow_for_10(a))
+    if (exp_a != PowOfTeen(a))
     {
-        printf("the num is %d and the return value is %f\n the expected num is %f\n", a, pow_for_10(a), exp_a);
+        printf("the num is %d and the return value is %f\n the expected num is %f\n"
+        	, a, PowOfTeen(a), exp_a);
         printf("ERROR\n");
     }
     else
     {
-        printf("the num is %d and the return value is %f\n the expected num is %f\n", a, pow_for_10(a), exp_a);
+        printf("the num is %d and the return value is %f\n the expected num is %f\n"
+        	, a, PowOfTeen(a), exp_a);
         printf("YESSSSSS\n");
     }
 
     return 0;
 }
 
-double pow_for_10(int n)
+double PowOfTeen(int n)
 {
     double base = 10;
     double ans = 1;
     int i;
+    
     if (n < 0)
     {
         base = (1 / base);
         n = -(n);
     }
-
+    
     for (i = 0; i < n; i++)
     {
         ans = base * ans;
     }
+    
     return ans;
 }
