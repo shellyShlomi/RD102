fd49636bf247fd1712bc5f6f5c0967147e05cc47
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-03-31 11:21:57 +0300

    ws8

---
+++ c/ws8/ex1.c
@@ -0,0 +1,6 @@
+
+/* Developer: Shelly Shlomi;									*/
+/* Status: in development;										*/
+/* Date Of Creation:31.03.21									*/
+/* Date Of Approval: --.--.--									*/
+/* Description: print array of struct befor and after addtion;	*/
---
+++ c/ws8/ex2_3_4.c
@@ -0,0 +1,7 @@
+/* Developer: Shelly Shlomi;										*/
+/* Status: in development;											*/
+/* Date Of Creation:31.03.21										*/
+/* Date Of Approval: --.--.--										*/
+/* Description: 2 - macro of MAX; 3 - SIZEOF_VAR;4 - SIZEOF_TYPE;	*/
+
+
