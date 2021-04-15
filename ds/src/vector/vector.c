b513bade97de1284c0235999e652636868087d4e
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-04-15 10:58:37 +0300

    vector done befoer approve

---
+++ ds/src/vector/vector.c
@@ -0,0 +1,170 @@
+/*  Developer: Shelly Shlomi;									*/
+/*  Status:DONE;										*/
+/*  Date Of Creation:14.04.21;									*/
+/*  Date Of Approval:--.--.21;									*/
+/*  Approved By: 												*/
+/*  Description: vector data structure 							*/
+
+#include <stdlib.h>	/* malloc */
+#include <assert.h> /* assert */
+#include <stdlib.h> /* exit status */
+
+#include "vector.h"
+
+#define FACTOR 2
+
+
+struct vector
+{
+	void **arr;
+	size_t capacity;
+	size_t size;
+}; 
+ 
+/* O(1) time, O(n)space */
+vector_t *VectorCreate(size_t capacity)
+{
+	vector_t *vector = (vector_t *)malloc(sizeof(vector_t));
+	
+	if (NULL == vector)
+	{
+		return NULL; 
+	}
+	
+	vector->arr = (void **)malloc(capacity * sizeof(void **));
+	if (NULL == vector->arr)
+	{
+		return NULL; 
+	}
+	
+	vector->size = 0;
+	vector->capacity = capacity;
+
+	return vector;
+}
+
+/* O(1) time, O(1)space */
+void VectorDestroy(vector_t *vector)
+{
+	assert(NULL != vector);
+
+	free(vector->arr);
+	vector->arr = NULL;
+	free(vector);
+	return ;
+}
+
+/* O(1) time, O(1)space */
+size_t VectorSize(const vector_t *vector)
+{
+	assert(NULL != vector);
+		
+	return vector->size;
+}
+
+
+/* O(1) time, O(1)space */
+size_t VectorCapacity(const vector_t *vector)
+{
+	assert(NULL != vector);
+	
+	return vector->capacity;
+}
+
+
+int VectorPushBack(vector_t *vector, void *data)
+{
+	/*int err = -1;*/
+	
+	assert(NULL != vector);
+	assert(NULL != data);
+
+	if (vector->size == vector->capacity)
+	{
+		/*err = VectorReserve(vector, vector->size * FACTOR);*/
+		
+		if (VectorReserve(vector, vector->size * FACTOR))
+		{
+			return EXIT_FAILURE; 
+		}
+	}
+	
+	++(vector->size);
+	VectorSetElem(vector, vector->size - 1, data);
+	
+	return EXIT_SUCCESS;
+}
+
+int VectorReserve(vector_t *vector, size_t new_capacity)
+{
+	void **arr_temp = NULL;
+	
+	assert(NULL != vector);
+	
+	arr_temp = (void **)realloc(vector->arr, sizeof(void **) * new_capacity);
+	
+	if (NULL == arr_temp)
+	{	
+		return EXIT_FAILURE; 
+	}
+
+	vector->arr = arr_temp;
+	
+	arr_temp  = NULL;
+	
+	vector->capacity = new_capacity;
+	
+	if (vector->size > vector->capacity)
+	{
+		vector->size = vector->capacity;
+	}
+	
+	return EXIT_SUCCESS;
+}
+
+
+int VectorShrinkToFit(vector_t *vector)
+{
+	assert(NULL != vector);
+	
+	vector->arr = (void **)realloc(vector->arr, sizeof(void **) * vector->size);
+	
+	if (NULL == vector->arr)
+	{	
+		return EXIT_FAILURE; 
+	}
+	
+	vector->capacity = vector->size;
+	
+	return EXIT_SUCCESS;
+}
+void VectorPopBack (vector_t *vector)
+{
+	assert(NULL != vector);
+	assert(0 != vector->size);
+	
+	*(vector->arr + vector->size - 1) = NULL;
+	--(vector->size);
+
+	return ;
+}
+
+void *VectorGetElem(const vector_t *vector, size_t index)
+{
+	assert(NULL != vector);
+	assert(index < vector->size);
+	
+	return *(vector->arr + index);	
+
+}
+
+void VectorSetElem(vector_t *vector, size_t index, void *data)
+{
+	assert(NULL != vector);
+	assert(index < vector->size);
+	
+	*(vector->arr + index) = data;
+	
+	return;
+}
+       
---
+++ ds/test/vector_test.c
@@ -0,0 +1,248 @@
+
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* exit status */
+#include <string.h> /* strcmp */
+
+#include "vector.h"
+
+#define CAPACITY 10
+#define CAPACITY_SMALL 2
+#define SUCCESS 0
+
+
+enum ELEM
+{
+	ZERO,
+	ONE,
+	TWO,
+	THREE,
+	FOUR, 
+	FIVE
+};
+
+
+static int VectorManager();
+	
+int main()
+{
+	int fail = 0;
+	
+	fail = VectorManager();
+	
+	if (fail)
+	{	
+		printf("VectorManager error at line: %d\n", __LINE__);
+		
+		return EXIT_FAILURE;
+	}
+	
+	return EXIT_SUCCESS;
+}
+
+static int VectorManager()
+{
+
+	float data = 20.33;
+	int data1 = 9;
+	double data2 = 601.33333;
+	char *str = "shelly";
+	char *str1 = "shelly1";
+	char *str2 = "shelly2";
+	char *str3 = "shelly3";
+	
+	
+	
+	vector_t *vector = VectorCreate(CAPACITY);
+	
+	if (NULL == vector)
+	{
+		printf("VectorCreate error at line: %d\n", __LINE__);
+		return EXIT_FAILURE; 
+	}
+	
+	if (CAPACITY != VectorCapacity(vector))
+	{
+		printf("VectorCapacity error at line: %d\n", __LINE__);
+	}
+	
+	if (ZERO != VectorSize(vector))
+	{
+		printf("VectorSize error at line: %d\n", __LINE__);
+	}
+
+	if (SUCCESS != VectorPushBack(vector, (void *)&data1))
+	{
+		printf("VectorPushBack error at line: %d\n", __LINE__);
+	}
+	
+	if (ONE != VectorSize(vector))
+	{
+		printf("VectorSize error at line: %d\n", __LINE__);
+	}
+	
+	if (SUCCESS != VectorPushBack(vector, (void *)&data))
+	{
+		printf("VectorPushBack error at line: %d\n", __LINE__);
+	}
+	
+	if (TWO != VectorSize(vector))
+	{
+		printf("VectorSize error at line: %d\n", __LINE__);
+	}
+	
+	if (SUCCESS != VectorPushBack(vector, (void *)str))
+	{
+		printf("VectorPushBack error at line: %d\n", __LINE__);
+	}
+	
+	if (str != (char *)VectorGetElem(vector, TWO))
+	{
+		printf("VectorGetElem error at line: %d\n", __LINE__);
+	}
+	
+	if (THREE != VectorSize(vector))
+	{
+		printf("VectorSize error at line: %d %ld\n", __LINE__, VectorSize(vector));
+	}
+
+
+	VectorSetElem(vector, TWO,(void *)&data2);
+	if (data2 != *(double *)VectorGetElem(vector, TWO))
+	{
+		printf("VectorSetElem error at line: %d\n", __LINE__);
+	}
+	
+	/* test if the size and the capacity changed after ShrinkToFit */
+	{
+		if (SUCCESS != VectorShrinkToFit(vector))
+		{
+			printf("VectorShrinkToFit error at line: %d\n", __LINE__);
+		}
+		
+		if (VectorCapacity(vector)	!= 	VectorSize(vector) &&
+							THREE 	== 	VectorSize(vector)		)
+		{
+			printf("VectorShrinkToFit error at line: %d\n", __LINE__);
+		}
+	}
+	/* test if the size was multyply after a push whene the arr was full */
+	{
+		if (SUCCESS != VectorPushBack(vector, (void *)str))
+		{
+			printf("VectorPushBack error at line: %d\n", __LINE__);
+		}
+		if ((VectorSize(vector) - 1) * TWO != VectorCapacity(vector))
+		{
+			printf("VectorPushBack & VectorReserve error line: %d\n", __LINE__);
+		}
+	}
+	
+		/* test the reserv func on extending & redusing the size */
+	{
+		if (SUCCESS != VectorReserve(vector, CAPACITY_SMALL))
+		{
+			printf("VectorReserve to small error at line: %d\n", __LINE__);
+		}
+		
+		if (VectorSize(vector)	!= 	VectorCapacity(vector) &&
+				CAPACITY_SMALL 	== 	VectorSize(vector)			)
+		{
+			printf("VectorReserve to small error at line: %d\n", __LINE__);
+		}
+		/* the top elem after Reserve down */
+		if (data != *(float *)VectorGetElem(vector, ONE))
+		{
+			printf("VectorGetElem error at line: %d \n", __LINE__);
+		}
+		
+		if (SUCCESS != VectorReserve(vector, CAPACITY))
+		{
+			printf("VectorReserve to a bigger error at line: %d\n", __LINE__);
+		}
+		
+		if (CAPACITY != VectorCapacity(vector) && TWO == VectorSize(vector))
+		{
+			printf("VectorReserve to a bigger error at line: %d\n", __LINE__);
+		}
+		/* the top elem after Reserve up */
+		if (data != *(float *)VectorGetElem(vector, ONE))
+		{
+			printf("VectorGetElem error at line: %d \n", __LINE__);
+		}
+	}
+	
+	/* test the VectorPopBack func */
+	{
+		if (SUCCESS != VectorPushBack(vector, (void *)str))
+		{
+			printf("VectorPushBack error at line: %d\n", __LINE__);
+		}
+		
+		if (SUCCESS != VectorPushBack(vector, (void *)str1))
+		{
+			printf("VectorPushBack error at line: %d\n", __LINE__);
+		}
+		
+		if (SUCCESS != VectorPushBack(vector, (void *)str2))
+		{
+			printf("VectorPushBack error at line: %d\n", __LINE__);
+		}
+		
+		if (str2 != (char *)VectorGetElem(vector, VectorSize(vector) - 1))
+		{
+			printf("VectorGetElem error at line: %d\n", __LINE__);
+		}
+		
+		if (SUCCESS != VectorPushBack(vector, (void *)str3))
+		{
+			printf("VectorPushBack error at line: %d\n", __LINE__);
+		}
+		
+		VectorPopBack(vector);
+		if (FIVE != VectorSize(vector))
+		{
+			printf("VectorPopBack to small error at line: %d\n", __LINE__);
+		}
+		
+		/* to see the val of the top elem after pop */
+		if (str2 != (char *)VectorGetElem(vector, VectorSize(vector) - 1))
+		{
+			printf("VectorPopBack error at line: %d\n", __LINE__);
+		}
+		
+		VectorPopBack(vector);
+		if (FOUR != VectorSize(vector))
+		{
+			printf("VectorPopBack to small error at line: %d\n", __LINE__);
+		}
+		/* to see the val of the top elem after pop */
+		if (str1 != (char *)VectorGetElem(vector, VectorSize(vector) - 1))
+		{
+			printf("VectorPopBack error at line: %d\n", __LINE__);
+		}
+		
+		VectorPopBack(vector);
+		if (THREE != VectorSize(vector))
+		{
+			printf("VectorPopBack to small error at line: %d\n", __LINE__);
+		}
+		
+		VectorPopBack(vector);
+		if (TWO != VectorSize(vector))
+		{
+			printf("VectorPopBack to small error at line: %d\n", __LINE__);
+		}
+		
+		if (data != *(float *)VectorGetElem(vector, ONE))
+		{
+			printf("VectorPopBack error at line: %d \n", __LINE__);
+		}
+	}
+	
+	VectorDestroy(vector);
+	vector = NULL;
+	
+	return EXIT_SUCCESS; 
+
+}
+
