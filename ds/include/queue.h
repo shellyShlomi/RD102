49c1b77bf19c5e6fb5b19c46606a5574f5078106
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-04-21 17:50:47 +0300

    empty files

---
+++ ds/include/queue.h
@@ -0,0 +1,24 @@
+/* 20.4.21 14:00 version_1 */
+
+#ifndef __OL102_QUEUE_H__
+#define __OL102_QUEUE_H__
+
+#include <stddef.h> /* size_t */
+
+typedef struct queue queue_t;
+
+queue_t *QueueCreate(void);                    				  /* O(1) */
+void QueueDestroy(queue_t *queue);				              /* O(n) */	
+
+size_t QueueSize(const queue_t *queue);         			  /* O(n) */ 
+int  QueueIsEmpty(const queue_t *queue);		              /* O(1) */
+
+int  QueueEnqueue(queue_t *queue, void *data);	              /* O(1) */
+void *QueuePeek(const queue_t *queue);						  /* O(1) */
+void QueueDequeue(queue_t *queue);							  /* O(1) */
+
+queue_t *QueueAppend(queue_t *dest, queue_t *src);            /* O(1) */
+ 
+#endif /* __OL102_QUEUE_H__ */
+
+
---
+++ ds/test/queue_test.c
@@ -0,0 +1,9 @@
+
+/*  Developer: Shelly Shlomi;									*
+ *  Status:in development;										*
+ *  Date Of Creation:21.04.21;									*
+ *  Date Of Approval:--.04.21;									*
+ *  Approved By: ;												*
+ *  Description: queue implementation as a client of 			*
+ *				 singly link list data structure;				*/
+
