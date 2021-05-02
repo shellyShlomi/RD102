a45e6d2b328658e5b9b0adde1d2f154f4e6bdb47
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-05-02 16:47:55 +0300

    sll_nth_element

--- quizzes/sll_2_5_21/sll_nth_element.c
+++ quizzes/sll_2_5_21/sll_nth_element.c
@@ -23,6 +23,7 @@ s_list_iter_t SLLnthElement(s_list_t *list, size_t n)
 	s_list_iter_t iter = NULL;
 	
 	assert(NULL != list);
+	assert(SLLCount(list) >= n);
 	
 	temp_iter = SLLBegin(list);
 	iter = temp_iter;
@@ -47,10 +48,10 @@ s_list_iter_t SLLnthElement(s_list_t *list, size_t n)
 static void Test()
 {
 	int arr[] = {4, 1, 6, 1, 7, 8};
+	int arr_n[] = {1, 2, 3, 4, 5, 6};
 	size_t size = sizeof(arr)/ sizeof(arr[0]);
 	
 	size_t i = 0;
-	size_t n = 3;
 	s_list_t *list = SLLCreate();
 	s_list_iter_t iter = NULL;
 
@@ -61,21 +62,59 @@ static void Test()
 		
 		return ; 
 	}
-	iter = SLLBegin(list); 
-	for (i = 0; i < size; ++i)
-	{
+	/* test on 1 elem list */
+	{	
+		iter = SLLBegin(list); 
 		SLLInsert(iter, (void *)(arr + i));
-		iter = SLLNext(iter);
-		
-	}
-	
-	iter = SLLBegin(list); 
-	
-	if ((arr[size - n]) != *(int *)SLLGetData(SLLnthElement(list, n)))
+
+		if ((arr[i]) != *(int *)SLLGetData(SLLnthElement(list, arr_n[i])))
 		{
 			printf("SLLnthElement error at line: ");
 			printf("%d \n", __LINE__); 
 		}
+	}
+	
+	
+	SLLDestroy(list);
+	list = SLLCreate();
+	
+	/* test on empty list */	
+	{	
+		if (*(int *)SLLGetData(SLLBegin(list)) != *(int *)SLLGetData(SLLnthElement(list, 0)))
+		{
+			printf("SLLnthElement error at line: ");
+			printf("%d \n", __LINE__); 
+		}
+	}
+	
+	/* test all indxes in a list list */	
+	{
+		iter = SLLBegin(list); 
+		
+		for (i = 0; i < size; ++i)
+		{
+			SLLInsert(iter, (void *)(arr + i));
+			iter = SLLNext(iter);
+			
+		}
+
+
+		iter = SLLBegin(list); 
+			
+		for (i = 0; i < size; ++i)
+		{
+			if ((arr[size - arr_n[i]]) != *(int *)SLLGetData(SLLnthElement(list, arr_n[i])))
+			{
+				printf("SLLnthElement error at line: ");
+				printf("%d \n", __LINE__); 
+			}
+			
+			iter = SLLNext(iter);
+			
+		}
+	}
+	
+		
 
 	SLLDestroy(list);
 	list = NULL;	
