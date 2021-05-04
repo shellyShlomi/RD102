06f07df6806c0918ff04c427e59e5bea150d67d4
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-05-04 15:41:47 +0300

    approved

--- ds/src/uid/uid.c
+++ ds/src/uid/uid.c
@@ -1,9 +1,9 @@
 
 /*  Developer: Shelly Shlomi;									*
- *  Status:in development ;										*
+ *  Status:Approved ;											*
  *  Date Of Creation:03.05.21;									*
- *  Date Of Approval:00.05.21;									*
- *  Approved By:												*
+ *  Date Of Approval:04.05.21;									*
+ *  Approved By: OHAD											*
  *  Description:unique id data structure;						*/
  
 #include <time.h>	/* time_t */
