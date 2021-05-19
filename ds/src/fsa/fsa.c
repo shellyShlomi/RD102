1232a0c81e2610848f3940b6b6d59e2350eaaada
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-05-19 11:48:50 +0300

    done

--- ds/src/fsa/fsa.c
+++ ds/src/fsa/fsa.c
@@ -1,3 +1,12 @@
+/*  Developer: Shelly Shlomi;									*
+ *  Status:done;												*
+ *  Date Of Creation:18.05.21;									*
+ *  Date Of Approval:--.05.21;									*
+ *  Approved By: ;												*
+ *  Description: fixed-size blocks allocation;					*/
+ 
+
+
 #include <stddef.h> /* size_t */
 #include <assert.h>
 
@@ -17,34 +26,47 @@ typedef struct fsa_block_header
 }fsa_block_header_t;
 
 
+
 fsa_t *FSAInit(void *mem_pool, size_t pool_size, size_t inner_block_size)
 {
-	fsa_block_header_t *block_header = mem_pool;
+	fsa_block_header_t *block_header = NULL;
 	size_t sum_block_offset = 0;
 	size_t align_diif = 0;
 	size_t num_of_blocks = 0;
-	size_t i = 1;
 	size_t modulo_res = (inner_block_size % WORDSIZE);
+	size_t modulo_align = ((size_t)mem_pool % WORDSIZE);
+	size_t i = 1;
 	
 	assert(NULL != mem_pool);
 	
-	align_diif = ((size_t)mem_pool % WORDSIZE);
 	
-	mem_pool = (char *)mem_pool + align_diif;	
+	align_diif = (WORDSIZE * (!!modulo_align)) - modulo_align;
+	
+	mem_pool = (fsa_block_header_t *)((char *)mem_pool + align_diif);	
 	inner_block_size += (WORDSIZE * (!!modulo_res)) - modulo_res;
 	
 	num_of_blocks = (pool_size - align_diif - sizeof(fsa_t)) / inner_block_size;
 	
+	block_header = mem_pool;
+	
+	if(0 == num_of_blocks)
+	{
+		return (NULL);
+	
+	}
+	
 	block_header->next_free = sizeof(fsa_t);
-	sum_block_offset = block_header->next_free;
-	block_header = block_header + sizeof(fsa_t);
-
-	for(i = 1; i < num_of_blocks; ++i)
+	sum_block_offset = sizeof(fsa_t);
+	block_header = (fsa_block_header_t *)((char *)block_header + sizeof(fsa_t));
+	
+	for(i = 0; i < num_of_blocks - 1; ++i)
 	{
 		block_header->next_free = (inner_block_size) + sum_block_offset;
 		sum_block_offset = block_header->next_free;
 		block_header = (fsa_block_header_t *)((char *)mem_pool + sum_block_offset);
 	}
+
+	block_header->next_free = 0;
 	
 	return ((fsa_t *)mem_pool);
 }
@@ -77,7 +99,7 @@ void FSAFree(fsa_t *fsa, void *mem_block)
 	
 	((fsa_block_header_t *)mem_block)->next_free = fsa->next_free;
 	
-	fsa->next_free = ((size_t)((fsa_t *)mem_block - fsa));
+	fsa->next_free = ((size_t)((char *)mem_block - (char *)fsa));
 	
 	return ;
 }
@@ -88,7 +110,7 @@ size_t FSASuggestSize(size_t num_of_blocks, size_t block_size)
 	
 	block_size += (WORDSIZE * (!!modulo_res)) - modulo_res;
 	
-	return ((block_size * num_of_blocks) + sizeof(fsa_t));
+	return ((block_size * num_of_blocks) + sizeof(fsa_t) + (WORDSIZE - 1));
 	
 }
 
--- ds/test/fsa_test.c
+++ ds/test/fsa_test.c
@@ -1,8 +1,222 @@
 
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* malloc */
+
+#include "fsa.h" 
+
+static void TestSuggestSize();
+static void TestInit();
+static void TestAllocAndCountFreeAndFree();
 
 int main()
 {
-
-
+	TestSuggestSize();
+	TestAllocAndCountFreeAndFree();
+	TestInit();
+	
 	return 0;
 }
+
+static void TestSuggestSize()
+{
+
+	size_t block_size[] = {4, 1, 6, 1, 7, 8};
+	size_t num_of_blocks[] = {1, 1, 4, 6, 7, 8};
+	size_t exp_size[] = {23, 23, 47, 63, 71, 79};
+	size_t i = 0;
+	size_t size = sizeof(exp_size)/ sizeof(exp_size[0]);
+	
+	for (i = 0; i < size; ++i)
+	{
+		if (exp_size[i] != FSASuggestSize(num_of_blocks[i], block_size[i]))
+			{
+				printf("TestSuggestSize->FSASuggestSize ");
+				printf("error at line: %d index: %ld\n", __LINE__,i); 	
+			}
+	}
+		
+	return ;
+}
+
+
+
+static void TestInit()
+{
+
+	size_t block_size[] = {4, 16, 1, 7, 8};
+	size_t pool_size[] = {100 ,100, 600, 700, 800};
+	size_t exp_block_size[] = {8, 16, 8, 8, 8};
+	size_t i = 0;
+	size_t j = 0;
+	size_t z = 1;
+	size_t size = sizeof(exp_block_size)/ sizeof(exp_block_size[0]);
+	fsa_t *init_res = NULL;
+	 
+	void *mem_pool = NULL;
+	
+	
+	for (i = 0; i < size - 1; ++i)
+	{
+		mem_pool = malloc(pool_size[i]);
+		init_res = FSAInit(((char *)mem_pool + i), pool_size[i], block_size[i]);
+		
+		if (1 == i)
+		{
+			
+				if (*((char *)init_res) !=  (8))
+				{
+					printf("TestInit-> FSAInit ");
+					printf("error at line: %d index: %ld, loop: %ld\n", __LINE__, j ,i); 	
+				}
+				
+				if (*((char *)init_res + 8) !=  24)
+				{
+					printf("TestInit-> FSAInit ");
+					printf("error at line: %d index: %ld, loop: %ld\n", __LINE__, j ,i); 	
+				}
+
+				if (*((char *)init_res + 24) !=  40)
+				{
+					printf("TestInit-> FSAInit ");
+					printf("error at line: %d index: %ld, loop: %ld\n", __LINE__, j ,i); 	
+				}
+
+		}
+		else
+		{
+			z = 1;
+			for (j = 0; j < size; ++j)
+			{
+				if ((size_t)(*((char *)init_res + (j * exp_block_size[i]))) !=  
+					(z * exp_block_size[i]))
+				{
+					printf("TestInit->FSAInit ");
+					printf("error at line: %d index: %ld, loop: %ld\n", __LINE__,j,i); 	
+				}
+				++z;
+			}
+		}
+			free(mem_pool);	
+	}
+	
+	mem_pool = malloc(pool_size[i]);
+	
+	if (mem_pool != FSAInit(mem_pool, pool_size[i], block_size[i]))
+	{
+		printf("TestInit->FSAInit ");
+		printf("error at line: %d index: %ld\n", __LINE__,i); 	
+	}
+	
+	free(mem_pool);	
+	return ;
+}
+
+
+
+
+static void TestAllocAndCountFreeAndFree()
+{
+
+	size_t block_size[] = {4, 16, 1, 7, 8};
+	size_t pool_size[] = {100 ,100, 600, 700, 800};
+	void *alloc_arr[] = {NULL, NULL, NULL, NULL};
+	size_t exp_count_free[] = {11, 5, 74, 86, 99};
+	size_t exp_alloc[] = {10, 9, 8, 7, 6};
+	size_t exp_free[] = {11, 10, 9, 8, 8, 9, 10, 11};
+	size_t size = sizeof(exp_count_free)/ sizeof(exp_count_free[0]);
+	size_t size_for_free = sizeof(exp_free)/ sizeof(exp_free[0]);
+	size_t j = 0;
+	size_t i = 0;
+
+	fsa_t *init_res = NULL;
+	 
+	void *mem_pool = NULL;
+	
+	/* test for FSACountFree
+	 */
+	{
+		for (j = 0; j < size; ++j)
+		{
+			mem_pool = malloc(pool_size[j]);
+			init_res = FSAInit(mem_pool, pool_size[j], block_size[j]);
+			
+			if (exp_count_free[j] != FSACountFree(init_res))
+			{
+				printf("TestAllocAndCountFreeAndFree->FSACountFree ");
+				printf("error at line: %d index: %ld\n", __LINE__,j); 	
+			}
+			
+			free(mem_pool);	
+		}
+	}
+	
+	/* test for FSAAlloc
+	 */
+	{
+		j = 0;
+		
+		mem_pool = malloc(pool_size[j]);
+		init_res = FSAInit(mem_pool, pool_size[j], block_size[j]);
+		
+		for (j = 0; j < size; ++j)
+		{
+			
+			FSAAlloc(init_res);
+		
+			if (exp_alloc[j] != FSACountFree(init_res))
+			{
+				printf("TestAllocAndCountFreeAndFree->FSAAlloc ");
+				printf("error at line: %d index: %ld\n", __LINE__,j); 	
+			}
+				
+
+		}
+		
+		free(mem_pool);	
+	}
+	
+	/* test for FSAFree
+	 */
+	{
+		j = 0;
+		
+		mem_pool = malloc(pool_size[j]);
+		init_res = FSAInit(mem_pool, pool_size[j], block_size[j]);
+		
+		for (j = 0; j < size_for_free / 2; ++j)
+		{
+			
+		
+			if (exp_free[j] != FSACountFree(init_res))
+			{
+				printf("TestAllocAndCountFreeAndFree->FSAAlloc ");
+				printf("error at line: %d index: %ld\n", __LINE__,j); 	
+			}
+				
+			alloc_arr[j] = FSAAlloc(init_res);
+
+		}
+		
+		
+		for (j = size_for_free / 2; j < size_for_free; ++j)
+		{
+			
+			FSAFree(init_res, alloc_arr[i]);
+		
+			if (exp_free[j] != FSACountFree(init_res))
+			{
+				printf("TestAllocAndCountFreeAndFree->FSAFree ");
+				printf("error at line: %d index: %ld\n", __LINE__,j); 	
+			}
+				
+			++i;
+		}
+		
+		free(mem_pool);	
+	}
+	
+	return ;
+}
+
+
+
