0af3670e33349d6149f13d6220ec0c5639d7beb4
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-06-02 19:50:29 +0300

    Sqrt quiz

---
+++ quizzes/Sqrt/sqrt.c
@@ -0,0 +1,57 @@
+#include <assert.h> /* assert */
+#include <math.h> /* fabs */
+#include <stdio.h>/* printf */
+
+#define PECISION_NUM (1e-5)
+
+static double Sqrt(double num); 
+static void TestSqrt();
+
+
+int main()
+{
+    TestSqrt();
+    
+    return 0;
+}
+
+static double Sqrt(double num) 
+{
+    double left = 0.0;
+    double right = (1 > num) ? 1 : num;
+    double mid = 0.0;
+    
+    assert(0 <= num);
+    
+    while(fabs(left - right) > PECISION_NUM) /* check whether the interval is small enough*/
+    { 
+        mid = (left + right) / 2;
+        if (mid * mid <= num)
+        {
+            left = mid;
+        } 
+        else
+        {
+            right = mid;
+        }
+    }
+
+    return (left);
+}
+
+static void TestSqrt()
+{	
+	double n = 0.0;
+	double jumps = 0.1;
+    
+    /* test all the numbers between 0 to 10000*/
+	while (10000 > n)
+	{
+		if (PECISION_NUM < fabs(sqrt(n) - Sqrt(n)))
+		{
+			printf("Test faild in number: %f,exp: %f, actual: %f \n at line: %d\n", n, sqrt(n), Sqrt(n), __LINE__); 	
+		}
+		
+		n += jumps;
+	}
+}
