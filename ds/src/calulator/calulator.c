866581cb3d6c5b630c6ac14e7dd4808a85db9150
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-06-05 19:49:10 +0300

    calulator empty files

---
+++ ds/src/calulator/calulator.c
@@ -0,0 +1,9 @@
+/*  Developer: Shelly Shlomi;									*
+ *  Status:;											        *
+ *  Date Of Creation:05.06.21;									*
+ *  Date Of Approval:--.06.21;									*
+ *  Approved By: ;							    	            *
+ *  Description:	calulator with moduls of: addition,         * 
+ *                  subtraction, multiplication, division       *	
+ *					multiplication, division			        */
+
