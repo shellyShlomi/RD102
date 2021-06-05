fbc9ac20adca27756dc7f1bbdf46643309911ce7
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-06-05 19:52:22 +0300

    calulator empty files

---
+++ ds/include/calculator.h
@@ -0,0 +1,7 @@
+#ifndef _OL_102_FSM_H_
+#define _OL_102_FSM_H_
+
+int Calculate(const char *expression, double *result);
+
+
+#endif /* _OL_102_FSM_H_ */
--- ds/src/calulator/calulator.c
+++ ds/src/calulator/calulator.c
@@ -7,3 +7,18 @@
  *                  subtraction, multiplication, division       *	
  *					multiplication, division			        */
 
+#include "calculate.h"
+
+typedef struct calculator
+{
+    stack_t *operands;
+    stack_t *operations;
+    state_t  transition[STATE][EVENT];
+}calculator_t;
+
+typedef enum state
+{     
+	WAIT_FOR_DIGIT,
+	WAIT_FOR_OPERATOR,     
+	ERROR_STATE 
+} state_t;
