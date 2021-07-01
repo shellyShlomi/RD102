4a54131e54262b4e911ec32ba7214796ca48bdfc
shelly.shlomi <shelly.shlomi@mail.huji.ac.il>
2021-07-01 12:52:43 +0300

    dhcp

--- ds/src/dhcp/dhcp.c
+++ ds/src/dhcp/dhcp.c
@@ -7,8 +7,232 @@
  *               Internet Protocol (IP) networks for            * 
  *               automatically assigning IP addresses           */
 
-
 #include <stddef.h> /* size_t */
 #include <assert.h>
+#include <stdlib.h>  /* malloc */
+#include <stdio.h>   /* printf */
+#include <endian.h>  /* LITTLE_ENDIAN */
 
 #include "dhcp.h"
+#include "trie.h"
+
+#define RESERVED_IPS (3)
+
+#define BITS_IN_4BYTES (32)
+#define IPV4 (4)
+#define BITS_IN_BYTE (8)
+#define LEST_BYTE (3)
+#define FAULT (0)
+#define TRUE (1)
+
+
+enum reserved_ips
+{
+    NETWORK_ADDRESS,
+    SERVER_ADDRESS,
+    BROADCAST_ADDRESS
+};
+
+typedef enum func
+{
+    FREE,
+    ALLOC
+} func_t;
+
+struct dhcp
+{
+    trie_t *trie;
+    unsigned long subnet_id;
+    unsigned long bit_of_claint;
+    unsigned long reserved_ip[RESERVED_IPS];
+};
+
+/*------------- helper funcs ------------*/
+static void CovertToArr(unsigned long ip, unsigned char new_ip[4]);
+static unsigned long CovertToLong(unsigned char ip[]);
+
+static int IsInSubnetRange(dhcp_t *dhcp, unsigned long ip);
+/*------------------------------ implementetion --------------------------------*/
+
+dhcp_t *DhcpCreate(const unsigned char subnet_id_bytes[4], size_t occupied_bits)
+{
+    unsigned long mask = ~0 << ((unsigned long)BITS_IN_4BYTES - (unsigned long)occupied_bits);
+    unsigned long returnd_ip = 0;
+    unsigned char new_ip[4] = {0};
+
+    dhcp_t *dhcp = NULL;
+
+    assert(subnet_id_bytes);
+
+    dhcp = (dhcp_t *)malloc(sizeof(dhcp_t));
+    if (!dhcp)
+    {
+        return (NULL);
+    }
+
+    dhcp->subnet_id = CovertToLong((unsigned char *)subnet_id_bytes) & mask;
+
+    dhcp->bit_of_claint = (unsigned long)(BITS_IN_4BYTES - occupied_bits);
+
+    dhcp->trie = TrieCreate((dhcp->bit_of_claint));
+
+    if (!dhcp->trie)
+    {
+        dhcp->subnet_id = 0;
+        dhcp->bit_of_claint = 0;
+
+        free(dhcp);
+        dhcp = NULL;
+    }
+
+    /*the reserved addres X .11111110 */
+    if (TrieInsert(dhcp->trie, ((~mask) ^ 0x1), &returnd_ip))
+    {
+        DhcpDestroy(dhcp);
+    }
+    /*the reserved addres X.11111111*/
+    if (TrieInsert(dhcp->trie, (~mask), &returnd_ip))
+    {
+        DhcpDestroy(dhcp);
+    }
+    /*the reserved addres X.000000000*/
+    if (TrieInsert(dhcp->trie, 0, &returnd_ip))
+    {
+        DhcpDestroy(dhcp);
+    }
+
+    dhcp->reserved_ip[NETWORK_ADDRESS] = 0;
+    dhcp->reserved_ip[SERVER_ADDRESS] = ((~mask) ^ 0x1);
+    dhcp->reserved_ip[BROADCAST_ADDRESS] = (~mask);
+
+    return (dhcp);
+}
+
+/* o(n) */
+size_t DhcpCountFree(const dhcp_t *dhcp)
+{
+    assert(dhcp);
+    assert(dhcp->trie);
+
+    return ((0x1 << dhcp->bit_of_claint) - TrieSize(dhcp->trie));
+}
+
+void DhcpDestroy(dhcp_t *dhcp)
+{
+    assert(dhcp);
+    assert(dhcp->trie);
+
+    TrieDestroy(dhcp->trie);
+
+    dhcp->trie = NULL;
+    dhcp->subnet_id = 0;
+    dhcp->bit_of_claint = 0;
+
+    free(dhcp);
+    dhcp = NULL;
+
+    return;
+}
+
+dhcp_status_t DhcpAllocteIp(dhcp_t *dhcp,
+                            const unsigned char optional_ip[4], unsigned char new_ip[4])
+{
+    int status = SUCCESS;
+    unsigned long returnd_ip = 0;
+    unsigned long ip = 0;
+
+    assert(new_ip);
+    assert(dhcp);
+
+    if (optional_ip)
+    {
+        ip = CovertToLong((unsigned char *)optional_ip);
+    }
+    else
+    {
+        ip = (~ip & dhcp->subnet_id) + 1;
+    }
+
+    if (!IsInSubnetRange(dhcp, ip))
+    {
+        return (NOT_IN_SUBNET_RANGE);
+    }
+    status = TrieInsert(dhcp->trie, ip, &returnd_ip);
+    CovertToArr(returnd_ip, new_ip);
+
+    return (status);
+}
+
+dhcp_status_t DhcpFreeIp(dhcp_t *dhcp, const unsigned char ip_address[4])
+{
+    unsigned long ip = 0;
+
+    assert(dhcp);
+    assert(ip_address);
+
+    ip = CovertToLong((unsigned char *)ip_address);
+
+    if (!IsInSubnetRange(dhcp, ip))
+    {
+        return (NOT_IN_SUBNET_RANGE);
+    }
+    if (IsIpFree(dhcp->trie, ip))
+    {
+        return (ALLREADY_FREED);
+    }
+
+    TrieRemove(dhcp->trie, ip);
+
+    return (SUCCESS);
+}
+
+static unsigned long CovertToLong(unsigned char ip[])
+{
+    unsigned long mask = 0;
+    size_t i = 0;
+
+    assert(ip);
+
+    for (i = 0; i < IPV4; ++i)
+    {
+        mask = mask << BITS_IN_BYTE;
+        mask |= ip[i];
+    }
+
+    return (mask);
+}
+
+static void CovertToArr(unsigned long ip, unsigned char new_ip[4])
+{
+    size_t i = 0;
+    assert(ip);
+    assert(new_ip);
+
+    for (i = 0; i < IPV4; ++i)
+    {
+        new_ip[IPV4 - i - 1] |= (unsigned char)ip;
+        ip = (ip) >> BITS_IN_BYTE;
+    }
+
+    return;
+}
+
+static int IsInSubnetRange(dhcp_t *dhcp, unsigned long ip)
+{
+    unsigned long mask = ~(~0 << dhcp->bit_of_claint);
+    unsigned long mask1 = ~0 << dhcp->bit_of_claint;
+    
+    assert(dhcp);
+
+    mask = mask & ip;
+    mask1 = mask1 & ip;
+
+    if ((mask1 ^ dhcp->subnet_id) ||
+        (mask >= (dhcp->reserved_ip[SERVER_ADDRESS]) ||
+        (mask <= (dhcp->reserved_ip[NETWORK_ADDRESS]))))
+    {
+        return (FAULT);
+    }
+
+    return (TRUE);
+}
---
+++ ds/src/trie/trie.c
@@ -0,0 +1,414 @@
+/*  Developer: Shelly Shlomi;									*
+ *  Status:in development;                                      *
+ *  Date Of Creation:15.06.21;									*
+ *  Date Of Approval:--.06.21;									*
+ *  Approved By:  ;	            								*
+ *  Description: trie DS                                        */
+
+#include <stdlib.h> /* malloc */
+#include <stddef.h> /* size_t */
+#include <string.h> /* memmove */
+#include <math.h>   /* pow */
+#include <assert.h>
+
+#include "dhcp.h"
+#include "trie.h"
+
+#define BITS_IN_4BYTES (32)
+#define BINARIY_BASE (2)
+
+#define MAX(hight_l, hight_r) ((hight_l) > (hight_r) ? (hight_l) : (hight_r))
+
+#define NEXT_NODE_BIT(bits, ip) ((ip >> ((bits)-1) & 0x1))
+#define SIBLING_BIT(x, i) (((~i) >> ((x)-1)) & 0x1)
+
+#define HAS_RIGHT_CHILD(node) ((node)->node_family[RIGHT])
+#define HAS_LEFT_CHILD(node) ((node)->node_family[LEFT])
+#define HAS_PARENT(node) ((node)->node_family[PARENT])
+
+#define IS_NEXT_NODE_FULL(node, ip, bit) ((node)->node_family[(bit)]->is_full)
+#define IS_NEXT_NODE_SIBLING_FULL(node, ip, bit) ((node)->node_family[SIBLING_BIT((bit), (ip))]->is_full)
+
+#define NEXT_NODE(node, bit) ((node)->node_family[(bit)])
+
+#define IS_RIGHT_CHILD_FULL(node) ((node)->node_family[RIGHT]->is_full)
+#define IS_LEFT_CHILD_FULL(node) ((node)->node_family[LEFT]->is_full)
+
+#define PARENT_NODE(node) ((node)->node_family[PARENT])
+
+struct trie_node
+{
+    trie_node_t *node_family[FAMILY_SIZE];
+    long is_full;
+};
+
+struct trie
+{
+    trie_node_t *root;
+    size_t num_of_bits;
+};
+
+/*------------- helper funcs ------------*/
+static long Count(void *unuesed, void *param);
+
+static void SetNode(trie_node_t *node, trie_node_t *left_child,
+                    trie_node_t *right_child, trie_node_t *parent, long is_full);
+
+/*------------- helper func of API wapres ------------*/
+
+static void TrieRecDestroy(trie_node_t *node);
+
+static size_t RecTrieSize(trie_node_t *node, size_t hight);
+
+/*------------- Insert ------------*/
+
+static dhcp_status_t InerInsert(trie_t *tree, unsigned long *wanted_ip, unsigned long *returned_ip);
+static trie_node_t *WraperGetIpNode(trie_t *tree, unsigned long wanted_ip, family_t *ip_child_side);
+static trie_node_t *GetIpNode(trie_node_t *node, unsigned long wanted_ip, unsigned long *bits, family_t *ip_child_side);
+static dhcp_status_t RecInsert(trie_node_t *node, unsigned long bits, unsigned long *wanted_ip, unsigned long *returned_ip);
+
+static dhcp_status_t CreatBranch(trie_node_t *node,
+                                 unsigned long *wanted_ip,
+                                 unsigned long bits,
+                                 unsigned long *returned_ip);
+static void DestroyBranch(trie_node_t *node, unsigned long wanted_ip, unsigned long bits);
+static void UpDateIsFull(trie_node_t *node);
+/*------------------------------ implementetion --------------------------------*/
+
+trie_t *TrieCreate(size_t num_of_bits)
+{
+    trie_t *tree = (trie_t *)malloc(sizeof(trie_t));
+
+    if (!tree)
+    {
+        return (NULL);
+    }
+
+    tree->root = (trie_node_t *)malloc(sizeof(trie_node_t));
+    if (!tree->root)
+    {
+        free(tree);
+        return (NULL);
+    }
+    tree->root->is_full = 0;
+    tree->num_of_bits = num_of_bits;
+
+    SetNode(tree->root, NULL, NULL, NULL, 0);
+
+    return (tree);
+}
+
+void TrieDestroy(trie_t *tree)
+{
+    assert(tree);
+
+    if (!tree->root)
+    {
+        tree->num_of_bits = -1;
+
+        free(tree);
+        tree = NULL;
+
+        return;
+    }
+
+    TrieRecDestroy(tree->root);
+    tree->num_of_bits = -1;
+    tree->root = NULL;
+
+    free(tree);
+    tree = NULL;
+
+    return;
+}
+
+dhcp_status_t TrieInsert(trie_t *tree, unsigned long wanted_ip, unsigned long *returned_ip)
+{
+
+    unsigned long ip = wanted_ip;
+
+    assert(tree);
+    assert(returned_ip);
+
+    *returned_ip = wanted_ip;
+
+    if (tree->root->is_full)
+    {
+        return (TRIE_IS_FULL);
+    }
+
+    return (InerInsert(tree, &ip, returned_ip));
+}
+
+size_t TrieSize(trie_t *tree)
+{
+    return (RecTrieSize(tree->root, tree->num_of_bits));
+}
+
+void TrieRemove(trie_t *tree, unsigned long ip)
+{
+    assert(tree);
+    DestroyBranch(tree->root, ip, tree->num_of_bits);
+    return;
+}
+
+/*-----------------------------helper_function--------------------------------*/
+
+/* generic set node */
+static void SetNode(trie_node_t *node, trie_node_t *left_child,
+                    trie_node_t *right_child, trie_node_t *parent, long is_full)
+{
+    assert(node);
+
+    node->node_family[LEFT] = left_child;
+    node->node_family[RIGHT] = right_child;
+    node->node_family[PARENT] = parent;
+
+    node->is_full = is_full;
+
+    return;
+}
+
+/*-----------------------------Destroy helper func------------------------------*/
+static void TrieRecDestroy(trie_node_t *node)
+{
+    if (!node)
+    {
+        return;
+    }
+
+    TrieRecDestroy(node->node_family[LEFT]);
+    TrieRecDestroy(node->node_family[RIGHT]);
+
+    SetNode(node, NULL, NULL, NULL, 0);
+
+    free(node);
+    node = NULL;
+
+    return;
+}
+
+/*-----------------------------insert helper_function--------------------------------*/
+static dhcp_status_t InerInsert(trie_t *tree, unsigned long *wanted_ip, unsigned long *returned_ip)
+{
+    trie_node_t *node = NULL;
+    unsigned long bits = 0;
+
+    int status = SUCCESS;
+
+    assert(tree);
+    assert(wanted_ip);
+    assert(returned_ip);
+
+    bits = tree->num_of_bits;
+    node = tree->root;
+
+    if ((status = RecInsert(node, bits, wanted_ip, returned_ip)) == MALLOC_FAILED)
+    {
+        TrieRemove(tree, *wanted_ip);
+    }
+    return (status);
+}
+
+static dhcp_status_t RecInsert(trie_node_t *node,
+                               unsigned long bits,
+                               unsigned long *wanted_ip,
+                               unsigned long *returned_ip)
+{
+    family_t ip_child_side = 0;
+
+    long status = SUCCESS;
+
+    assert(node);
+    assert(wanted_ip);
+    assert(returned_ip);
+
+    node = GetIpNode(node, *wanted_ip, &bits, &ip_child_side);
+    if (!node->node_family[ip_child_side])
+    {
+        status = CreatBranch(node, wanted_ip, bits, returned_ip);
+        return (status);
+    }
+
+    *wanted_ip >>= bits;
+    *wanted_ip <<= bits;
+    *wanted_ip |= (0x1 << (bits - 1));
+
+    *returned_ip = *wanted_ip;
+
+    if (node->node_family[PARENT] && node->node_family[ip_child_side]->is_full && (RIGHT == ip_child_side))
+    {
+        node = node->node_family[PARENT];
+    }
+    if (node->node_family[RIGHT])
+    {
+        node = node->node_family[RIGHT];
+        --bits;
+    }
+
+    return (RecInsert(node, bits, wanted_ip, returned_ip));
+}
+
+static trie_node_t *GetIpNode(trie_node_t *node,
+                              unsigned long wanted_ip,
+                              unsigned long *bits,
+                              family_t *ip_child_side)
+{
+    trie_node_t *curr_node = NULL;
+    assert(node);
+    assert(ip_child_side);
+    assert(bits);
+
+    *ip_child_side = NEXT_NODE_BIT(*bits, wanted_ip);
+
+    while (*bits > 0 && node->node_family[*ip_child_side] && !node->node_family[*ip_child_side]->is_full)
+    {
+        node = node->node_family[*ip_child_side];
+        --*bits;
+        *ip_child_side = NEXT_NODE_BIT(*bits, wanted_ip);
+    }
+
+    return (node);
+}
+
+static trie_node_t *WraperGetIpNode(trie_t *tree, unsigned long wanted_ip, family_t *ip_child_side)
+{
+    trie_node_t *node = NULL;
+    unsigned long bits = 0;
+
+    assert(tree);
+
+    node = tree->root;
+    bits = tree->num_of_bits;
+
+    return (GetIpNode(node, wanted_ip, &bits, ip_child_side));
+}
+
+static dhcp_status_t CreatBranch(trie_node_t *node,
+                                 unsigned long *wanted_ip,
+                                 unsigned long bits,
+                                 unsigned long *returned_ip)
+{
+    trie_node_t *malloced_node = NULL;
+    unsigned long ip = 0;
+
+    assert(wanted_ip);
+
+    ip = *wanted_ip;
+
+    if (!NEXT_NODE(node, NEXT_NODE_BIT(bits, *wanted_ip)))
+    {
+        while (bits > 0)
+        {
+            malloced_node = (trie_node_t *)malloc(sizeof(trie_node_t));
+            if (!malloced_node)
+            {
+                return (MALLOC_FAILED);
+            }
+
+            node->node_family[NEXT_NODE_BIT(bits, *wanted_ip)] = malloced_node;
+            SetNode(malloced_node, NULL, NULL, node, 0);
+            node = NEXT_NODE(node, NEXT_NODE_BIT(bits, *wanted_ip));
+            --bits;
+        }
+    }
+    else if (NEXT_NODE(node, NEXT_NODE_BIT(bits, *wanted_ip))->is_full)
+    {
+        return (RecInsert(node, bits, wanted_ip, returned_ip));
+    }
+
+    if (node == malloced_node && bits == 0)
+    {
+        node->is_full = 1;
+    }
+
+    UpDateIsFull(node);
+
+    return (SUCCESS);
+}
+
+static void UpDateIsFull(trie_node_t *node)
+{
+    while (node)
+    {
+        if ((HAS_RIGHT_CHILD(node) && HAS_LEFT_CHILD(node)) &&
+            IS_RIGHT_CHILD_FULL(node) &&
+            IS_LEFT_CHILD_FULL(node))
+        {
+            node->is_full = 1;
+        }
+
+        node = PARENT_NODE(node);
+    }
+}
+
+/*-----------------------------Size helper func--------------------------------*/
+
+static size_t RecTrieSize(trie_node_t *node, size_t hight)
+{
+    size_t size = 0;
+
+    if (!node)
+    {
+        return (0);
+    }
+    if (node->is_full)
+    {
+        return (0x1 << hight);
+    }
+
+    size += RecTrieSize(node->node_family[LEFT], hight - 1);
+    size += RecTrieSize(node->node_family[RIGHT], hight - 1);
+
+    return (size);
+}
+
+/*-----------------------------Remove helper func--------------------------------*/
+
+static void DestroyBranch(trie_node_t *node, unsigned long wanted_ip, unsigned long bits)
+{
+    unsigned long bits_left = bits;
+
+    if (!node)
+    {
+        return;
+    }
+
+    if (!bits_left)
+    {
+        return;
+    }
+
+    node->is_full = 0;
+
+    DestroyBranch(node->node_family[NEXT_NODE_BIT(bits_left, wanted_ip)], wanted_ip, bits_left - 1);
+
+    if (node->node_family[NEXT_NODE_BIT(bits_left, wanted_ip)] &&
+        node->node_family[NEXT_NODE_BIT(bits_left, wanted_ip)]->is_full)
+    {
+        free(node->node_family[NEXT_NODE_BIT(bits_left, wanted_ip)]);
+        node->node_family[NEXT_NODE_BIT(bits_left, wanted_ip)] = NULL;
+    }
+
+    return;
+}
+
+int IsIpFree(trie_t *tree, unsigned long wanted_ip)
+{
+    unsigned long bits = 0;
+    family_t ip_child_side = 0;
+    trie_node_t *node = NULL;
+
+    assert(tree);
+
+    node = tree->root;
+    bits = tree->num_of_bits;
+    node = GetIpNode(node, wanted_ip, &bits, &ip_child_side);
+
+    if (node->node_family[ip_child_side] && node->node_family[ip_child_side]->is_full)
+    {
+        bits = 0;
+    }
+    
+    return (bits);
+}
